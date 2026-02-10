c9a7082e7cd71f0a787d2e6c6e3a0a9e44cdda1f
vitabachynska <vitabachynska@gmail.com>
2026-02-10 10:29:49 +0200

    Видалила непотрібну папку

--- Pr3/tasks/src/InitOrderTask.java
+++
@@ -1,9 +0,0 @@
-public class InitOrderTask {
-    static void run(){
-        new ChildInit();
-
-    }
-    public static void main(String[] args){
-        run();
-    }
-}
--- Pr3/tasks/src/UserRepository.java
+++
@@ -1,9 +0,0 @@
-import java.util.Optional;
-public class UserRepository {
-    public Optional<User> findByEmail(String email) {
-        if ("test@email.com".equals(email)) {
-            return Optional.of(new User(email));
-        }
-        return Optional.empty();
-    }
-}
